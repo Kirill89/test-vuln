6dc584a89c5503502e42a8598fc08d4edefe76e9
Kirill Efimov <kirill89@gmail.com>
2022-12-19 17:31:49 +0200

    x

--- src/main/java/SQLInjectionExample.java
+++ src/main/java/SQLInjectionExample.java
@@ -3,8 +3,9 @@ import java.sql.*;
 public class SQLInjectionExample {
   public static void main(String[] args) throws SQLException {
     String userInput = args[1];
+    String userInput2 = args[2];
 
-    String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+    String query = "SELECT * FROM users WHERE username = '" + userInput + "' AND password = '" + userInput2 + "'";
     Statement stmt = conn.createStatement();
     ResultSet rs = stmt.executeQuery(query);
 
